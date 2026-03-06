603965b3e8f1e96a17028843cbdf5376f723c103
M-begue <begue.matteo24@gmail.com>
2026-03-06 10:28:48 +0100

    Installation du projet

---
+++ Mattéo_Hubert_App/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Mattéo_Hubert_App;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
